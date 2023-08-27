961fc99bf772931527cb705ffa4db90ca6b73c17
Dinoboros <meryl.valier@gmail.com>
2023-08-28 00:32:34 +0200

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,15 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Méryl VALIER on 28/08/2023.
+//
+
+import Foundation
+
+/// Represents unique API endpoint
+@frozen enum RMEndpoint: String {
+    case character
+    case location
+    case episode
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Méryl VALIER on 28/08/2023.
+//
+
+import Foundation
+
+/// Object that represent a single API Call
+final class RMRequest {
+    
+}
