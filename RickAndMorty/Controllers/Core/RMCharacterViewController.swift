8896fab708235fa6c7d44a66335428defa836942
Dinoboros <meryl.valier@gmail.com>
2023-08-28 21:48:04 +0200

    Added collection view to character tab - ep6

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 /// Controller to show and search for character
 final class RMCharacterViewController: UIViewController {
+    
+    private let characterListView = RMCharacterListView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,16 +18,13 @@ final class RMCharacterViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
         title = "Characters"
-        
-        RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResponse.self) { result in
-            switch result {
-            case .success(let character):
-                print("Total: \(character.info.count)")
-                print("Page result count : \(character.results.count)")
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
+        view.addSubview(characterListView)
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
 
---
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  RMCharacterListViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Méryl VALIER on 28/08/2023.
+//
+
+import UIKit
+
+final class CharacterListViewViewModel: NSObject {
+    func fetchCharacters() {
+        RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResponse.self) { result in
+            switch result {
+            case .success(let character):
+                print("Total: \(character.info.count)")
+                print("Page result count : \(character.results.count)")
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
+    }
+}
+
+extension CharacterListViewViewModel: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .brown
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width - 30) / 2
+
+        return CGSize(width: width, height: width * 1.5)
+    }
+}
